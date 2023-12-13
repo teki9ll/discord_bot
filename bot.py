9dc18ca866c55af75d591125a904708fedb0fc76
Tejas Solanke <95670904+teki9ll@users.noreply.github.com>
2023-12-13 23:33:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,7 +97,6 @@ async def on_message(message):
     else:
         await send_message(message, user_message, is_private=False)
 
-# Add the following lines to your existing code
 TOKEN = "MTE4NDQ3MzE2NDQ0MTQ2ODk4OA.G-aZ-T._ldDnn4wcIqzI0qBg6z-aIHPwgxs4vDBjzpDWA"
 
 intents = discord.Intents.default()
